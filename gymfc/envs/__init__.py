3169315a030b149c46c6ce5cb800b682e4678cd1
StarMoon <50798962+moneystarmoon@users.noreply.github.com>
2019-06-24 15:57:32 +0800

    Delete __init__.py

--- gymfc/envs/__init__.py
+++
@@ -1,4 +0,0 @@
-from gymfc.envs.attitude_control_env import GyroErrorFeedbackEnv
-from gymfc.envs.attitude_control_env import GyroErrorESCVelocityFeedbackEnv
-from gymfc.envs.attitude_control_env import GyroErrorESCVelocityFeedbackContinuousEnv
-from gymfc.envs.attitude_control_env import GoToPositionEnv
